2d83180baf1f07cd1dd1324955ef50cc8064633c
sugoxalr <TaehyeonEntus@gmail.com>
2024-06-04 13:09:20 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        System.out.println((year%400==0)||((year%4==0)&&(year%100!=0))?"1":"0");
+    }
+}
