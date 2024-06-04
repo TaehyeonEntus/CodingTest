f5127fa88eb6801e5dc752b0b73f2db01865c036
sugoxalr <TaehyeonEntus@gmail.com>
2024-06-04 11:19:16 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextInt();
+        double b = scanner.nextInt();
+        System.out.println(a / b);
+    }
+}
