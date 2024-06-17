7b084578e3d87e4d21864ea704f8c49843b5f42f
sugoxalr <TaehyeonEntus@gmail.com>
2024-06-17 20:47:45 +0900

    [Bronze V] Title: A+B - 7, Time: 260 ms, Memory: 18664 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Pair {
+    int A;
+    int B;
+
+    Pair(int A, int B) {
+        this.A = A;
+        this.B = B;
+    }
+
+    int Sum() {
+        return A + B;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        int arrayLength = Integer.parseInt(string);
+
+        ArrayList<Pair> list = new ArrayList<>();
+
+        for (int i = 0; i < arrayLength; i++) {
+            string = scanner.nextLine();
+            String[] strings = string.split(" ");
+
+            int A = Integer.parseInt(strings[0]);
+            int B = Integer.parseInt(strings[1]);
+            Pair pair = new Pair(A, B);
+            list.add(pair);
+        }
+        for(int i = 0; i<arrayLength; i++){
+            System.out.println("Case #"+(i+1)+": "+list.get(i).Sum());
+        }
+    }
+}
