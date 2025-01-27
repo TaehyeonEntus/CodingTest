4afa67eda1ac76a2c278761cd910a26c126ebe1c
sugoxalr <TaehyeonEntus@gmail.com>
2025-01-27 22:12:10 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.07 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        StringBuilder even = new StringBuilder();
+        StringBuilder odd = new StringBuilder();
+        for(int i : num_list){
+            if(i%2==0)
+                even.append(i);
+            else
+                odd.append(i);
+        }
+        answer += Integer.parseInt(even.toString());
+        answer += Integer.parseInt(odd.toString());
+        return answer;
+    }
+}
