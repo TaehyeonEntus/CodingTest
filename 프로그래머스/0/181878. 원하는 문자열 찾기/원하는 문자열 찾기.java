ca806eef6426417e27baac57a48c06480a5af2f8
sugoxalr <TaehyeonEntus@gmail.com>
2025-01-28 15:51:01 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.92 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        return myString
+            .toLowerCase()
+            .contains(pat.toLowerCase())
+            ? 1:0;
+        
+    }
+}
