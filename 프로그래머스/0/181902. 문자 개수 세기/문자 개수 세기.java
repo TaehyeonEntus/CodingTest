014a6baf648f3d760d448ef8454e5cd51155dae9
sugoxalr <TaehyeonEntus@gmail.com>
2025-02-16 17:08:47 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.13 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for(int i = 0; i<my_string.length(); i++){
+            if(my_string.charAt(i)<=90&&my_string.charAt(i)>=65)
+                answer[my_string.charAt(i)-65]++;
+            else
+                answer[my_string.charAt(i)-71]++;
+        }
+        return answer;
+    }
+}
