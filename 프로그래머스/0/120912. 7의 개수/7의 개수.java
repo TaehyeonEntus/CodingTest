7f5b722c1467ace47bc90a4cc910e2f1187a81d6
sugoxalr <TaehyeonEntus@gmail.com>
2025-03-03 19:21:09 +0900

    [level 0] Title: 7의 개수, Time: 0.08 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i : array){
+            String str = String.valueOf(i);
+            for(char c : str.toCharArray())
+                if(c == '7')
+                    answer++;
+        }
+        return answer;
+    }
+}
