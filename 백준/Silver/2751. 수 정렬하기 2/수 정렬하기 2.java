938378a788d934fef6a8f5bdcea84eb6b4a44adb
sugoxalr <TaehyeonEntus@gmail.com>
2025-07-27 20:15:11 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1400 ms, Memory: 169412 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+       Collections.sort(list);
+
+        for(Integer i : list) {
+            sb.append(i);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
