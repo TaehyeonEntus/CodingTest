e07c9b7f111adda364a377b316925427cffcca0b
sugoxalr <TaehyeonEntus@gmail.com>
2025-08-05 16:21:58 +0900

    [Silver III] Title: N과 M (3), Time: 616 ms, Memory: 205692 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+import com.sun.source.tree.Tree;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Deque<Integer> queue = new ArrayDeque<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        dfs(n, m, 0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int n, int m, int depth) {
+        if (depth == m) {
+            queue.forEach(e -> sb.append(e).append(" "));
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+            dfs(n, m, depth + 1);
+            queue.removeLast();
+        }
+    }
+}
+
