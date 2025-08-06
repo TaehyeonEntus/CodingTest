fdb51e7bc1bef2838b7c1eb6797448a2e76fd6f5
sugoxalr <TaehyeonEntus@gmail.com>
2025-08-06 13:04:02 +0900

    [Silver III] Title: 01타일, Time: 140 ms, Memory: 18128 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        if (n > 1)
+            dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] %= 15746;
+        }
+        System.out.println(dp[n]);
+    }
+}
+
