1ff87a42f5bd5e33592deb6f9f1cf3a043ef780e
sugoxalr <TaehyeonEntus@gmail.com>
2025-08-11 13:54:04 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 740 ms, Memory: 109828 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,17 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
-    static int[] dp;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         char[] str = br.readLine().toCharArray();
-        dp = new int[str.length+1];
+        int strLength = str.length;
+
+        dp = new int[26][strLength];
+
+
+        dp[str[0] - 'a'][0] = 1;
+
+        for (int i = 1; i < strLength; i++) {
+            for (int c = 0; c < 26; c++) {
+                dp[c][i] = dp[c][i - 1];
+            }
+            dp[str[i] - 'a'][i]++;
+        }
+
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
@@ -19,21 +31,16 @@ public class Main {
             char targetChar = s[0].charAt(0);
             int start = Integer.parseInt(s[1]);
             int end = Integer.parseInt(s[2]);
-            dp[0] = 0;
 
-            if (targetChar == str[0])
-                dp[1] = 1;
-            else
-                dp[1] = 0;
-
-            for (int j = 2; j < str.length+1; j++) {
-                if (targetChar == str[j-1])
-                    dp[j] = dp[j - 1] + 1;
-                else
-                    dp[j] = dp[j - 1];
+            int idx = targetChar - 'a';
+            int answer = dp[idx][end];
+            if (start > 0) {
+                answer -= dp[idx][start - 1];
             }
 
-            System.out.println(dp[end+1]-dp[start]);
+            sb.append(answer).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
