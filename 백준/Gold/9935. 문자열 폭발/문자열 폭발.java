672a41508da85f3806d610e20fbe99e9497d7af9
sugoxalr <TaehyeonEntus@gmail.com>
2025-10-03 20:11:25 +0900

    [Gold IV] Title: 문자열 폭발, Time: 340 ms, Memory: 40896 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,37 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static Deque<Character> stack = new ArrayDeque<>();
     static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         String s = br.readLine();
         String bomb = br.readLine();
-        char endChar = bomb.charAt(bomb.length() - 1);
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        //bomb trigger
+        char trigger = bomb.charAt(bomb.length() - 1);
+
+        for (char c : s.toCharArray()) {
             sb.append(c);
-            if (c == endChar) {
+            if (c == trigger) {
                 int start = sb.length()-bomb.length();
                 int end = sb.length();
 
-                if(start < 0)
-                    continue;
+                if(start < 0) continue;
 
-                if(sb.substring(start, end).toString().equals(bomb))
+                if(sb.substring(start, end).equals(bomb))
                     sb.delete(start, end);
             }
         }
 
-        if (sb.length() == 0)
-            System.out.println("FRULA");
-        else
-            System.out.println(sb);
+        if (sb.isEmpty()) System.out.println("FRULA");
+        else System.out.println(sb);
     }
 }
-
